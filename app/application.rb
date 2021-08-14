07f6f3344d902578c658d5ecb314102b5f5eead6
Eric Nganga <ericmainaedits@gmail.com>
2021-08-13 23:31:24 -0500

    added 3 routes

--- app/application.rb
+++ app/application.rb
@@ -3,28 +3,46 @@ class Application
   def call(env)
     res = Rack::Response.new
     req = Rack::Request.new(env)
-  
 
-    
+   #Quote Index Route
+    if req.path == '/quotes' && req.get?
+      quotes = Quote.all
+      return [200, 
+      { 'Content-Type' => 'application/json' }, 
+      [quotes.to_json]]
+    end
 
-    #Quote Index Route
+    if req.path == '/characters' && req.get?
+      characters = Character.all
+      return [200, 
+      { 'Content-Type' => 'application/json' }, 
+      [characters.to_json]]
+    end
 
-    if req.path.match('/quotes') && req.get?
-      return [
-        200, 
-        { 'Content-Type' => 'application/json' }, 
-        [ Quote.all.to_json ]
-      ]
+    if req.path.match('/characters') && req.get?
+      id= req.path.split('/')[2]
+      character = Character.find_by(id: id)
+      if character
+        quotes = character.quotes
+        character_res = { 
+          name: character.name,
+          quotes: quotes
+        }
+        return [
+          200,
+          { 'Content-Type' => 'application/json' },
+          [ character_res.to_json ]
+        ]
+      else
+       return [
+          204,
+          { 'Content-Type' => 'application/json' },
+          [ { error: 'character not found'}.to_json]
+        ]
+      end
     end
   
-
-    #Create
-
-    if req.path.match('/quotes') && req.post?
-      body = JSON.parse(req.body.read)
-      quote = Quote.create(body)
-      return [201, {'Content-Type' => 'application/json'}, [quote.to_json ]]
-    end
+  
 
     #static route to test rack
     if req.path.match(/test/)
@@ -33,6 +51,8 @@ class Application
       res.write "Path Not Found"
 
     end
+
+    res.finish
   end
 end
 
