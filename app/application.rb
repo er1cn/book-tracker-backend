4a25c38682b20142ca2a9f4e86e42544921b2ae2
Eric Nganga <ericmainaedits@gmail.com>
2021-08-14 14:39:01 -0500

    recreated database, seeded new data

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,7 @@ class Application
       [quotes.to_json]]
     end
 
+    #Character Index Route
     if req.path == '/characters' && req.get?
       characters = Character.all
       return [200, 
@@ -19,10 +20,11 @@ class Application
       [characters.to_json]]
     end
 
+
     if req.path.match('/characters') && req.get?
       id= req.path.split('/')[2]
       character = Character.find_by(id: id)
-      if character
+     if character
         quotes = character.quotes
         character_res = { 
           name: character.name,
@@ -40,6 +42,24 @@ class Application
           [ { error: 'character not found'}.to_json]
         ]
       end
+    end
+
+    #Quote Create 
+    if req.path.match(/quotes/) && req.post?
+      body = JSON.parse(req.body.read)
+      quote = Quote.create(body)
+      return [200, 
+      { 'Content-Type' => 'application/json' }, 
+      [quote.to_json]]
+    end
+
+    #Character Create Route
+    if req.path.match(/characters/) && req.post?
+      body = JSON.parse(req.body.read)
+      character = Character.create(body)
+      return [200, 
+      { 'Content-Type' => 'application/json' }, 
+      [character.to_json]]
     end
   
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,67 @@
-c1 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c2 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c3 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c4 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c5 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c6 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c7 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c8 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c9 = Character.create(name: Faker::TvShows::FamilyGuy.character)
-c10 = Character.create(name: Faker::TvShows::FamilyGuy.character)
+
+c1 = Character.create(name: "Peter Griffin")
+c2 = Character.create(name: "Brian Griffin")
+c3 = Character.create(name: "Lois Griffin")
+c4 = Character.create(name: "Meg Griffin")
+c5 = Character.create(name: "Chris Griffin")
+c6 = Character.create(name: "Stewie Griffin")
+c7 = Character.create(name: "Glenn Quagmire")
+c8 = Character.create(name: "Joe Swanson")
+c9 = Character.create(name: "Cleveland Brown")
+c10 = Character.create(name: "Tom Tucker")
 
 
-10.times do
-Quote.new(
-    text: Faker::TvShows::FamilyGuy.quote,
-    rating: (0..10).to_a.sample,
-    character: [c1, c2, c3, c4].sample
-)
-end
+
+q1 = Quote.create(
+    id: '', 
+    text: "I haven't felt this great in years! I feel like Arnold Schwarzenegger, except without the fruity accent or the Pirates of the Caribbean wife.", 
+    rating: '5', 
+    character_id: '1')
+q2 = Quote.create(
+    id: '', 
+    text: "You know, it's times like this where I think if I didn't talk and you were a normal baby, we wouldn't have any of these problems.", 
+    rating: '9', 
+    character_id: '2')
+q3 = Quote.create(
+    id: '', 
+    text: "Peter, we have a hamper. Stop throwing your dirty clothes in the toilet.", 
+    rating: '5', 
+    character_id: '3')
+q4 = Quote.create(
+    id: '', 
+    text: "I cut your name into my arm so I can always remember you.", 
+    rating: '4', 
+    character_id: '4')
+q5 = Quote.create(
+    id: '', 
+    text: "I thought you were this wordly, sophisticated guy, but you're just a fraud. Like Moses.", 
+    rating: '5', 
+    character_id: '5')
+q6 = Quote.create(
+    id: '', 
+    text: "Alright I guess this is the night b****es die.", 
+    rating: '5', 
+    character_id: '6')
+q7 = Quote.create(
+    id: '', 
+    text: "I'm an idiot? All anyone remembers from your argument is that you're in a wheelchair!", 
+    rating: '5', 
+    character_id: '7')
+q8 = Quote.create(
+    id: '', 
+    text: "Peter, face it. Without Quagmire, we're boring.", 
+    rating: '5', 
+    character_id: '8')
+q9 = Quote.create(
+    id: '', 
+    text: "Oh, yeah. Black guys put hot sauce on everything on account most of us have been pepper-sprayed by the time we're two.", 
+    rating: '5', 
+    character_id: '9')
+q10 = Quote.create(
+    id: '', 
+    text: "Do you know who I am? I'm Tom Tucker, dammit! I make more in an hour than you make in two hours!", 
+    rating: '5', 
+    character_id: '10')
 
 puts 'Quotes Seeded'
 
